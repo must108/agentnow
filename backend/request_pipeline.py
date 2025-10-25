498ff6b362cf45e0192f896eec056fea1e0844c0
must108 <136145181+must108@users.noreply.github.com>
2025-10-25 16:45:43 -0400

    hack csv rag completed (basic)

--- backend/request_pipeline.py
+++ backend/request_pipeline.py
@@ -12,26 +12,25 @@ api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client()
 
 TOP_K = 3
-VEC_PATH = "data/accel_vectors.npy"
-TXT_PATH = "data/accel_text.txt"
+VEC_PATH = "data/user_vectors.npy"
+TXT_PATH = "data/user_text.txt"
 
 try:
-    df = pd.read_csv("data/accelerators.csv", encoding="utf-8")
+    df = pd.read_csv("data/u_hack.csv", encoding="utf-8")
 except UnicodeDecodeError:
-    df = pd.read_csv("data/accelerators.csv", encoding="cp1252")
+    df = pd.read_csv("data/u_hack.csv", encoding="cp1252")
 
 texts = []
-cols = ['name', 'description']
-
+cols = ["number", "capability", "company", "description", "initiative_title", "primary_category"]
 texts = [convert(row, cols) for _, row in df.iterrows()]
 
 DOMAIN_TOKENS = {}
 
-with open("data/accel_tokens.txt", "r") as fp:
+with open("data/hack_tokens.txt", "r") as fp:
     tokens = fp.read().splitlines()
     DOMAIN_TOKENS = set(tokens)
 
-tokenize = lambda s: set(re.findall(r"[a-z0-9]+", s.lower()))
+tokenize = lambda x: set(re.findall(r"[a-z0-9]+", x.lower()))
 
 embeddings_matrix, cache = load_cache(VEC_PATH, TXT_PATH)
 
@@ -39,19 +38,20 @@ if embeddings_matrix is None or cache != texts:
     vec = fully_embed(client, texts, "RETRIEVAL_DOCUMENT", True)
     vec = normalize(vec)
 
-    save_cache(VEC_PATH, TXT_PATH, vec, texts) 
+    save_cache(VEC_PATH, TXT_PATH, vec, texts)
     embeddings_matrix, _ = load_cache(VEC_PATH, TXT_PATH)
 
 while True:
-    inp = input("Ask a question: ")
+    inp = input("Query this dataset: ")
     if not inp: break
 
     if not (tokenize(inp) & DOMAIN_TOKENS):
-        print("\nWe don't seem to have an accelerator for this use case yet!")
-        continue
+        print("\nLess precise input")
 
     q_vec = fully_embed(client, [inp], "RETRIEVAL_QUERY", True)
     q_vec = normalize(q_vec)
+    if q_vec.ndim == 1:
+        q_vec = q_vec[None, :]
 
     if q_vec.shape[1] != embeddings_matrix.shape[1]:
         vec = fully_embed(client, texts, "RETRIEVAL_DOCUMENT", True)
@@ -66,10 +66,10 @@ while True:
     best_score = float(similarities[best_idx])
 
     if best_score < 0.15:
-        print("\nWe dont seem to have an accelerator for this use case yet!")
+        print("\nWe don't seem to have a user request for this use case yet!")
         continue
 
-    print("\nThe best accelerators for you are:")
+    print("\nThe best user requests for you are:")
     for i in idxs:
         if len(texts[i]) > 120:
             print(f"{texts[i][:120]}...")
